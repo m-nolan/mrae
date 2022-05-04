8a6d98347da8526ab0be1b8ec33882c7c913b873
Michael Nolan <manolan@uw.edu>
2022-05-04 15:12:27 -0700

    Add: encoder, decoder classes and tests (outputs are the right size, there may be computation errors...

--- mrae/MRAE.py
+++ mrae/MRAE.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from . import rnn
+
+from warnings import warn
 
 class MRAE(nn.Module):
 
@@ -23,10 +26,10 @@ class RAE_block(nn.Module):
     def __init__(self):
         super().__init__()
         # initalize models
-        self.rand_samp = True
-        self.encoder = Encoder()
-        self.decoder = Decoder()
-        self.block_out = None
+        self.rand_samp  = True
+        self.encoder    = Encoder()
+        self.decoder    = Decoder()
+        self.block_out  = None
 
     def forward(self,src):
         # forward pass
@@ -42,19 +45,68 @@ class RAE_block(nn.Module):
 
 class Encoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, input_size, output_size, hidden_size, dropout, num_layers=1, bidirectional=True):
         super().__init__()
+        self.input_size     = input_size
+        self.output_size    = output_size
+        self.hidden_size    = hidden_size
+        self.dropout        = dropout
+        self.num_layers     = num_layers
+        self.bidirectional  = bidirectional
+        
+        self.rnn    = nn.GRU(
+            input_size,
+            hidden_size,
+            num_layers,
+            batch_first=True,
+            dropout=dropout,
+            bidirectional=bidirectional
+        )
+
+        self.dropout_layer = nn.Dropout(dropout)
+
+        bidir_scale = 2 if bidirectional else 1
+        gru_output_size = bidir_scale * num_layers * hidden_size
+        if gru_output_size == 2 * output_size:
+            self.linear_out = nn.Identity()
+        else:
+            self.linear_out = nn.Linear(in_features=gru_output_size,out_features=2*output_size)
 
     def forward(self,input):
-        pass
+        batch_size, seq_len, input_size = input.size()
+        assert input_size == self.input_size
+        rnn_out, rnn_n = self.rnn(input)
+        #TODO: add rnn output clip
+        rnn_n = self.dropout_layer(rnn_n)
+        rnn_n = rnn_n.permute(1,0,2).reshape(batch_size,-1) # [batch_size, num_layers * bidir_scale * hidden_size]
+        param_out = self.linear_out(rnn_n)
+        mean, logvar = torch.split(param_out,self.output_size,dim=-1)
+        return mean, logvar
 
 class Decoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, hidden_size, dropout, num_layers=1, bidirectional=False):
         super().__init__()
+        self.input_size     = 1 # only >1 if there's a controller, not used in MRAE as-is
+        self.hidden_size    = hidden_size # equal to size of encoder outputs
+        self.dropout        = dropout
+        if num_layers > 1:
+            warn('Multi-layer decoder not currently supported. Defaulting to single layer.')
+        self.num_layers     = 1
+        if bidirectional:
+            warn('Bidirectional decoder not currently supported. Defaulting to unidirectional.')
+        self.bidirectional  = False
 
-    def forward(self,input):
-        pass
+        self.rnn    = rnn.GRU_Modified(self.input_size,self.hidden_size)
+        self.dropout_layer = nn.Dropout(dropout)
+
+    def forward(self,input,h0):
+        gen_out = self.rnn(input,h0=h0)
+        gen_out = self.dropout_layer(gen_out)
+        return gen_out
+
+    def gen_input(self,batch_size,seq_len):
+        return torch.zeros(batch_size,seq_len,self.input_size)
 
 def sample_gaussian(mean, logvar):
     """
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,62 @@
 import unittest
 import torch
-from mrae import rnn
+from mrae import MRAE, rnn
+
+class RAETests(unittest.TestCase):
+    
+    def test_mrae(self):
+        pass
+
+    def test_rae_block(self):
+        pass
+
+    def test_encoder(self):
+        input_size = 10
+        output_size = 10
+        hidden_size = 30
+        enc_dropout = 0.3
+        num_layers = 2 # try more layers later
+        bidirectional = True # try false, you won't
+
+        enc = MRAE.Encoder(
+            input_size=input_size,
+            output_size=output_size,
+            hidden_size=hidden_size,
+            dropout=enc_dropout,
+            num_layers=num_layers,
+            bidirectional=bidirectional
+        )
+
+        batch_size = 5
+        sequence_length = 20
+        input = torch.randn(batch_size,sequence_length,input_size)
+
+        mean, logvar = enc(input)
+        self.assertEqual(mean.size(),(batch_size,output_size))
+        self.assertEqual(logvar.size(),(batch_size,output_size))
+
+    def test_decoder(self):
+        hidden_size = 10
+        dec_dropout = 0.3
+        num_layers = 1
+        bidirectional = False
+
+        dec = MRAE.Decoder(
+            hidden_size=hidden_size,
+            dropout=dec_dropout,
+            num_layers=num_layers,
+            bidirectional=bidirectional
+        )
+
+        batch_size = 8
+        sequence_length = 50
+        input = dec.gen_input(batch_size,sequence_length)
+        h0 = torch.randn(batch_size,hidden_size)
+
+        dec_out = dec(input,h0)
+
+        self.assertEqual(dec_out.size(),(batch_size,sequence_length,hidden_size))
+
 
 class RNNTests(unittest.TestCase):
 
