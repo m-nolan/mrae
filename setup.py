827a9ad355206326ac53d1dfa978409438664019
Michael Nolan <manolan@uw.edu>
2022-05-11 18:48:49 -0700

    premerge

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     version="0.1.0",
     author="Michael Nolan",
     author_email="mnolan@uw.edu", # I am not eternal, please replace ~ MN
-    description="pytorch implementation of the M/RAE time series reconstruction model",
+    description="pytorch implementation of the MRAE time series reconstruction model",
     long_description=long_description,
     url='',
     packages=setuptools.find_packages(),
